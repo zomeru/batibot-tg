cbfe93df9cb8210ae7fbb65aa144b72d9ad59d9d
zomeru <zomerzxc.19@gmail.com>
2023-05-29 21:06:15 +0800

    Add some tweaks

--- src/utils/gpt.ts
+++ src/utils/gpt.ts
@@ -8,8 +8,12 @@ type User = {
 };
 
 export const chatCompletion = async (user: User, prompt: string) => {
-  const zomerInfo =
-    'Zomer is a Software Engineer based in the Philippines and the creator of this bot. You can check out his website at https://zomeru.com.';
+  const otherInfo =
+    'Skilled in React, Node, TypeScript and other web technologies. You can check out his website at https://zomeru.com.';
+
+  const zomerInfo = `Zomer is a Software Engineer based in the Philippines and the creator of this bot. ${otherInfo}`;
+
+  const hannahInfo = `Hannah is the girlfriend of creator of this bot (Zomer), Zomer is a Software Engineer based in the Philippines. ${otherInfo}`;
 
   // Check if user exists in database
   const { data: currentUser } = await supabase
@@ -60,7 +64,7 @@ export const chatCompletion = async (user: User, prompt: string) => {
         role: 'system',
         content: `Respond to the user's messages as best, accurately, convincingly, and as human-like as you can, keep it short and straight to the point. Maximum of 5 sentences. You can make it long if the users asks for it. You can also add bullets and numbers in a list in addition to the 5 sentences, but add them only if the user asks for it. You can try to call their name sometimes if you want, here's the user's name: ${
           user.firstName || user.username
-        }. Also, if they ask if you know or who Zomer, Zomer Gregorio, or zomeru is (that's me by the way, who created this bot (Batibot, name of the bot)), you can try to respond this message instead: "${zomerInfo}", you can try to add your own response in addition to that.${
+        }. Also, if they ask if you know or who Zomer, Zomer Gregorio, or zomeru is (that's me by the way, who created this bot (Batibot, name of the bot)), you can try to respond this message instead: "${zomerInfo}", or if they ask if you know or who Hannah Julieta Cabalo, Hannah Julieta, Hannah Cabalo, or bonk is (That's my girlfriend by the way), you can respond this message instead: "${hannahInfo}", you can try to add your own response in addition to that.${
           formattedMessages
             ? ` Also, you can try to make your answer based on the user's recent messages and your response (as an assistant) to those recent messages (conversation history), if they did not get the answer they want and they ask again. Here are the 20 recent messages of the user, the most recent is always number 1:\n\n${formattedMessages}`
             : ''
